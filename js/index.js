7ebb504b49c0c4f54431634c71e6340e293c8359
guitagun-3502 <37022012+guitagun-3502@users.noreply.github.com>
2019-10-24 21:13:32 +0300

    Full Version

--- js/index.js
+++ js/index.js
@@ -4,13 +4,19 @@
             var servicesBlock = $('.services-block'),
                   servicesInfo = $('.services-info'),
                   myWorksInfo = $('.my-works-info'),
+                  myWorksBlock = $('.my-works-block'),
+                  contactInfo = $('.contact-info'),
+                  input = $('input'),textarea = $('textarea'),
+                  contactButton = $('.contact-button')
                   win = $(window),
                   windowHeight = win.height();
             win.scroll(function (){
                   var scrollPos = $(this).scrollTop(),
                         servicesBlockPos = servicesBlock.offset().top,
                         servicesInfoPos = servicesInfo.offset().top,
-                        myWorksInfoPos = myWorksInfo.offset().top;
+                        myWorksInfoPos = myWorksInfo.offset().top,
+                        myWorksBlockPos = myWorksBlock.offset().top,
+                        contactInfoPos = contactInfo.offset().top;
                   if (scrollPos >= servicesBlockPos - windowHeight){
                         servicesBlock.addClass('apperance');
                   }
@@ -20,7 +26,33 @@
                   if (scrollPos >= myWorksInfoPos - windowHeight){
                         myWorksInfo.addClass('emergence');
                   }
+                  if (scrollPos >= myWorksBlockPos - windowHeight){
+                        myWorksBlock.addClass('sudden-appearance');
+                  }
+                  if (scrollPos >= contactInfoPos - windowHeight){
+                        contactInfo.addClass('emergence');
+                        input.addClass('input-animation');
+                        textarea.addClass('input-animation');
+                        contactButton.addClass('input-animation');
+                  }
+            });
+
+            $('.hamb').click(function(){
+                  if ($(this).attr('status') === 'false'){
+                        $('.a-menu').css('display', 'block');
+                        $('nav').addClass('hamb-animation');
+                        $('.a-menu').addClass('hamb-opacity');
+                        $(this).attr('status', 'true');
+                        $('nav').removeClass('hamb-animation-back');
+                        $('.a-menu').removeClass('hamb-opacity-back');
+                  }else{
+                        $('nav').addClass('hamb-animation-back');
+                        $('.a-menu').addClass('hamb-opacity-back');
+                        $('.a-menu').css('display', 'none');
+                        $(this).attr('status', 'false');
+                        $('nav').removeClass('hamb-animation');
+                        $('.a-menu').removeClass('hamb-opacity');
+                  }
             });
       })
-
 })();
